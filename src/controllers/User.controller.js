818600690e2c3a0cd7d5f342fa654b700f07ff34
souvik666 <peacedynasty0@gmail.com>
2021-11-12 18:02:26 +0530

    get by id

--- src/controllers/User.controller.js
+++ src/controllers/User.controller.js
@@ -1,60 +1,3 @@
-/* const User = require("../models/User.model");
-const express = require("express");
-const router = express.Router();
-const multer = require("multer");
-const path = require("path");
-
-function errHandler(err, req, res, next) {
-  if (err instanceof multer.MulterError) {
-    res.json({
-      success: 0,
-      message: err.message,
-    });
-  }
-}
-router.use(errHandler);
-const storage = multer.diskStorage({
-  destination: "./upload/images",
-  filename: (req, file, cb) => {
-    return cb(
-      null,
-      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
-    );
-  },
-});
-
-const upload = multer({
-  storage: storage,
-});
-router.use("/avatar", express.static("upload/images"));
-
-router.post("/", upload.single("avatar"), async function (req, res, next) {
-  const proImg = req.file.filename;
-  const newEntry = await User.create({
-    name: req.body.name,
-    phone: req.body.phone,
-    email: req.body.email,
-    image: `http://localhost:2345/avatar/${proImg}`,
-    gender: req.body.gender,
-  });
-  res.send({
-    newEntry,
-    proFileimgurl: `http://localhost:2345/avatar/${proImg}`,
-  });
-});
-
-router.patch("/:id", upload.single("avatar"), async function (req, res, next) {
-  const proImg = req.file.filename;
-  const whoHeis = await User.findByIdAndUpdate(req.params.id, {
-    name: req.body.name,
-    phone: req.body.phone,
-    email: req.body.email,
-    image: proImg,
-    gender: req.body.gender,
-  });
-  const whoishenow = await User.findById(req.params.id);
-  res.send({ whoishenow });
-}) */
 const User = require("../models/User.model");
 const express = require("express");
 const router = express.Router();
@@ -105,6 +48,11 @@ router.patch("/:id", upload.single("profile"), async (req, res) => {
 
 router.get("/", async (req, res) => {
   const Alluser = await User.find();
+  return res.send({ Alluser });
+});
+
+router.get("/:id", async (req, res) => {
+  const user = await User.findById(req.params.id);
   return res.send({ Alluser });
 });
 
